4d212af333d6859c86becc0473d3eeab1d6f1f64
kirasayaka <kirasayaka@localhost>
2025-06-07 17:33:59 +0900

    mission0 localhostで実行すると真っ白の画面になる

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -22,9 +22,9 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Table(name = "users")
--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,15 +1,12 @@
 package com.example.demo.service;
 
-import java.math.BigInteger;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo.dto.UserDailyTimestampDTO;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
