0cfe12ea2430ad3fc46f08b2f93511b3b0e0c5bb
lastd <lastd@host.docker.internal>
2025-07-04 20:46:21 +0900

    インデント修正

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,18 +1,17 @@
 //時計機能
 
 //数字が一桁の場合、先頭に0を追加する関数
-function twoFormat(data){
-  let number;
-  if(data < 10){
-    number = "0" + data;
-  }
-  else{
-    number = data;
-  }
-  return number;
+function twoFormat(data) {
+    let number;
+    if (data < 10) {
+        number = "0" + data;
+    } else {
+        number = data;
+    }
+    return number;
 }
 
-function clock(){
+function clock() {
     //現在時刻
     const nowTime = new Date();
     //年
@@ -32,47 +31,49 @@ function clock(){
     const nowSeconds = twoFormat(nowTime.getSeconds());
     
     const message = year + "/" + month + "/" + day + "(" + week + ")";
-	const message2 = nowHour + ":" + nowMinutes + ":" + nowSeconds;
+    const message2 = nowHour + ":" + nowMinutes + ":" + nowSeconds;
     document.getElementById("date").innerHTML = message;
-	document.getElementById("time").innerHTML = message2;
+    document.getElementById("time").innerHTML = message2;
 }
+
 //リアルタイム時間の表示
 setInterval('clock()',1000);
 
 
-//layout.html
+//layout.htmlの記載部分
+
 $(function(){
     let $nav = $("#navigation"),
-    $slideLine = $("#slide-line"),
-    $currentItem = $(".current-item");
+        $slideLine = $("#slide-line"),
+        $currentItem = $(".current-item");
+
     // メニューにアクティブな項目がある場合
     if ($currentItem.length) {
         $slideLine.css({
             "width": $currentItem.width() + 10 + "px",
             "left": $currentItem.position().left + 5 + "px"
-            });
+        });
     }
-    
+
     // 下線のトランジション
     $nav.find("li").hover(
         function(){
             $slideLine.css({
                 "width": $(this).width() + 10 + "px",
                 "left": $(this).position().left + 5 + "px"
+            });
+        },
+        function(){
+            if ($currentItem.length) {
+                // 現在の項目に戻す
+                $slideLine.css({
+                    "width": $currentItem.width() + 10 + "px",
+                    "left": $currentItem.position().left + 5 + "px"
                 });
-                },
-                function(){
-                    if ($currentItem.length) {
-                        // 現在の項目に戻す
-                        $slideLine.css({
-                            "width": $currentItem.width() + 10 + "px",
-                            "left": $currentItem.position().left + 5 + "px"
-                            });
-                    } else {
-                        // 非表示にする
-                        $slideLine.width(0);
-                    }
-                }
-            );
-        });
-
+            } else {
+                // 非表示にする
+                $slideLine.width(0);
+            }
+        }
+    );
+});
