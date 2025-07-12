dc45edf85ac48aa7ab4f4bcccce6d38fe178940c
kirasayaka <kirasayaka@localhost>
2025-07-12 16:26:27 +0900

    部署新規登録画面の実装

--- src/main/java/com/example/demo/Controller/DepatmentController.java
+++ src/main/java/com/example/demo/Controller/DepatmentController.java
@@ -1,5 +1,42 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Department;
+import com.example.demo.form.DepartmentForm;
+import com.example.demo.service.DepartmentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class DepatmentController {
+	
+	private final DepartmentService departmentService;
+
+    @GetMapping("/department/create")
+    private String showCreationForm(@ModelAttribute("departmentForm") DepartmentForm form) {
+        return "/department/create";
+    }
+    
+    @PostMapping("/department/create")
+    public String createDepartment(@Validated @ModelAttribute("departmentForm") DepartmentForm form,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            return "/department/create";
+        }
+
+        Department createDepartment = new Department();
+        createDepartment.setNameJp(form.getDepartmentname_jp());
+        createDepartment.setNameEn(form.getDepartmentname_en());
+        departmentService.createDepartment(createDepartment);
+
+        return "redirect:/login";
+    }
 
 }
--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public interface DepartmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
 }
