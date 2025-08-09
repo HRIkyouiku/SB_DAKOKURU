bb21f7f5e4567ae98d00dd92072a7b19b71484de
m-hirono <hiromado24@gmail.com>
2025-08-09 19:45:17 +0900

    部署追加機能のエラー確認

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
 
-public interface DepartmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long>  {
+
+	Department findByNameJp(String nameJp);
+
+	Department findByNameEn(String nameEn);
 
 }
--- src/main/java/com/example/demo/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/demo/service/DepartmentServiceImpl.java
@@ -1,5 +1,40 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+
 public class DepartmentServiceImpl implements DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+  //  @Override
+//    public List<Department> departmentlist(Long name_jp) {
+
+ //       List<Department> list = DepartmentRepository.departmentlist(name_jp);
+
+//        return list;
+   // }
+    
+    @Override
+    public void save(Department department) {
+        departmentRepository.save(department);
+    }
+    
+    @Override
+    public Department findByNameJp(String nameJp) {
+        return departmentRepository.findByNameJp(nameJp);
+    }
+
+    @Override
+    public Department findByNameEn(String nameEn) {
+        return departmentRepository.findByNameEn(nameEn);
+    }
 
 }
