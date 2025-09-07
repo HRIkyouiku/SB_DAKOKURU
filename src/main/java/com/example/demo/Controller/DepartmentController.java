fec32b8c40a0991193d45144c68eb4b2550f637b
小林千華 <kobayashichika@pc>
2025-09-07 22:43:49 +0900

    部署登録にバリデーションをかけました

--- src/main/java/com/example/demo/Controller/DepartmentController.java
+++ src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -4,10 +4,15 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Department;
+import com.example.demo.form.DepartmentForm;
 import com.example.demo.repository.DepartmentRepository;
 
 @Controller
@@ -18,24 +23,37 @@ public class DepartmentController {
         this.departmentRepository = departmentRepository;
     }
     @GetMapping("/department/create")
-    public String showCreateForm(Model model) {
-        model.addAttribute("department", new Department());
+    public String create(Model model) {
+        if (!model.containsAttribute("departmentForm")) {
+        	model.addAttribute("departmentForm", new DepartmentForm());
+        }
         return "department/create";  
     }
     @PostMapping("/department/create")
-    public String createDepartment(Department department) {
-    	LocalDateTime now = LocalDateTime.now();
+    public String createDepartment(
+    		@Validated @ModelAttribute("departmentForm") DepartmentForm form,
+    		BindingResult result,
+    		RedirectAttributes ra) {
     	
-    	if (department.getCreatedAt() == null) {
-            department.setCreatedAt(now);
-        }
-        if (department.getUpdatedAt() == null) {
-            department.setUpdatedAt(now);
-        }
-        if (department.getDeletedAt() == null) {
-            department.setDeletedAt(null);
-        }
+    	if(result.hasErrors()) {
+    		ra.addFlashAttribute("org.springframework.validation.BindingResult.departmentForm",result);
+    		ra.addFlashAttribute("departmentForm",form);
+    		return "redirect:/department/create";
+    	}
+    	
+    	Department department = new Department();
+        department.setNameJp(form.getNameJp());
+        department.setNameEn(form.getNameEn());
+        LocalDateTime now= LocalDateTime.now();
+        department.setCreatedAt(now);
+        department.setUpdatedAt(now);
+
+        // データベースに保存
         departmentRepository.save(department);
+
+        // リダイレクト時にメッセージを追加
+        ra.addFlashAttribute("successMessage", "部署が登録されました。");
+
         return "redirect:/department/list";
     }
     @GetMapping("/department/list")
