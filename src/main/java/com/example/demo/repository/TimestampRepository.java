36622434d5544466d65e4e2001ceaaadc903d23d
HANA101544\gurif <kh86101544@gmail.com>
2025-09-23 14:32:33 +0900

    Repositoryにページネーション対応を追加：
    ・戻り値をPage<Timestamp>に変更し、ページ構造で履歴をDBから取得
    ・引数にPageableを追記しController、Serviceからのページ指定に対応させる

--- src/main/java/com/example/demo/repository/TimestampRepository.java
+++ src/main/java/com/example/demo/repository/TimestampRepository.java
@@ -1,13 +1,17 @@
 package com.example.demo.repository;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Timestamp;
 
 public interface TimestampRepository extends JpaRepository<Timestamp, Long> {
 
-    List<Timestamp> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+    Page<Timestamp> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+    Optional<Timestamp> findTopByUserIdOrderByDateDescTimeDesc(Long userId);
 
 }
