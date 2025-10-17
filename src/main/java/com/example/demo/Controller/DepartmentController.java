6683e96cccfaf6dd9a2427f94f3dfeea349240e1
nam <nam@macbookair.adach1.kt.home.ne.jp>
2025-10-17 14:54:28 +0900

    バリデーション新規エラーコメント

--- src/main/java/com/example/demo/Controller/DepartmentController.java
+++ src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -5,13 +5,18 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Department;
+import com.example.demo.form.DepartmentForm;
+import com.example.demo.form.ValidationGroups.DepartmentCreateGroup;
 import com.example.demo.service.DepartmentService;
 
 import lombok.RequiredArgsConstructor;
@@ -38,18 +43,29 @@ public class DepartmentController {
     //新規登録画面表示-------------------------
     @GetMapping("/department/create")
     public String createDepartment(Model model) {
-        model.addAttribute("department", new Department());
+        if (!model.containsAttribute("departmentForm")) {
+            model.addAttribute("departmentForm", new DepartmentForm());
+        }
         return "/department/create";
     }
     //新規登録処理-------------------------
     @PostMapping("/save")
-    public String saveDepartment(@ModelAttribute Department departments) {
-        departmentService.saveDepartment(departments);
+    public String saveDepartment(@Validated(DepartmentCreateGroup.class) @ModelAttribute("departmentForm") DepartmentForm form,
+            BindingResult result,
+            RedirectAttributes ra) {
+        if (result.hasErrors()) {
+            ra.addFlashAttribute("org.springframework.validation.BindingResult.departmentForm", result);
+            ra.addFlashAttribute("departmentForm", form);
+            return "redirect:/department/create";
+        }
+        Department createDepartment= new Department();
+        createDepartment.setNameJp(form.getNameJp());
+        createDepartment.setNameEn(form.getNameEn());
         return "redirect:/department/index";
     }
     //編集フォーム表示
     @GetMapping("department/edit/{id}")
-    public String editDepartment(@PathVariable Long id, Model model) {
+    public String editDepartment(@PathVariable Long id, Model model, @ModelAttribute("departmentForm") DepartmentForm form) {
         Optional<Department> department = departmentService.findById(id);
         model.addAttribute("department", department.get());
         return "department/edit";
@@ -61,9 +77,20 @@ public class DepartmentController {
         return "redirect:/department/index";
     }
     
+    //編集機能
     @PostMapping("/department/update")
-    public String updateDepartment(@ModelAttribute Department departments) {
-        departmentService.save(departments);
+    public String updateDepartment(@ModelAttribute("departmentForm") DepartmentForm form, Model model) {
+        Optional<Department> department = departmentService.findById(form.getId());
+        Department oldDepartment = department.get();
+        String newNameJp = form.getNameJp();
+        if(newNameJp != null && !newNameJp.isEmpty()) {
+            oldDepartment.setNameJp(newNameJp);
+        }
+        String newNameEn = form.getNameEn();
+        if(newNameEn != null && !newNameEn.isEmpty()) {
+            oldDepartment.setNameEn(newNameEn);
+        }
+        departmentService.save(oldDepartment);
         return "redirect:/department/index";
     }
 }
---
+++ src/main/java/com/example/demo/form/DepartmentForm.java
@@ -0,0 +1,22 @@
+package com.example.demo.form;
+
+import jakarta.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
+public class DepartmentForm implements ValidationGroups {
+    
+    private Long id;
+    
+    @NotBlank(message = "部署名を入力してください。", groups = DepartmentCreateGroup.class)
+    @Length(min = 1, max = 255, message = "部署名は1文字以上、255文字以内で入力してください。", groups = {DepartmentCreateGroup.class, DepartmentUpdateGroup.class})
+    private String nameJp;
+    
+    @NotBlank(message = "部署名（英語）を入力してください。", groups = DepartmentCreateGroup.class)
+    @Length(min = 1, max = 255, message = "部署名（英語）は1文字以上、255文字以内で入力してください。", groups = {DepartmentCreateGroup.class, DepartmentUpdateGroup.class})
+    private String nameEn;
+
+}
