eec0e343b6f36588d6a8fe7d674063c660358320
Mai <Mai@host.docker.internal>
2025-11-10 23:50:34 +0900

    部署一覧・編集・追加の修正

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -13,6 +13,9 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -27,9 +30,14 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message="部署名を入力してください。")
+    @Size(min=1, max=255, message="部署名は1文字以上、255文字以内で入力してください。")
     @Column(name = "name_jp")
     private String nameJp;
 
+    @NotEmpty(message="部署名（英語）を入力してください。")
+    @Size(min=1, max=255, message="部署名（英語）は1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp="^[a-zA-Z0-9]+$", message="部署名（英語）は半角英数字で入力してください。")
     @Column(name = "name_en")
     private String nameEn;
 
--- src/main/java/com/example/demo/service/DepartmentService.java
+++ src/main/java/com/example/demo/service/DepartmentService.java
@@ -1,5 +1,19 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.entity.Department;
+
 public interface DepartmentService {
 
+	List<Department> departmentList();
+	public void storeDepartment(Department department);
+	Department findById(Long id);
+	Department updateDepartment(Long id, Department department);
+	void deleteById(Long id);
+	boolean existsByNameJp(String nameJp);
+	boolean existsByNameEn(String nameEn);
+	boolean existsByNameJpAndIdNot(String nameJp, Long id);
+	boolean existsByNameEnAndIdNot(String nameEn, Long id);
+	List<Department> findByNameJpContainingOrNameEnContaining(String nameJp, String nameEn);
 }
