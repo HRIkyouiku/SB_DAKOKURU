3b999e6fc6a2163118899a732c3c9a657a0714c0
Mai <Mai@host.docker.internal>
2025-11-17 13:46:01 +0900

    script.jsの修正

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,57 @@
+function setupNavigationUnderline() {
+	let $nav = $("#navigation"),
+		$slideLine = $("#slide-line"),
+		$currentItem = $(".current-item");
+
+	// メニューにアクティブな項目がある場合
+	if ($currentItem.length) {
+		$slideLine.css({
+		width: $currentItem.width() + 10 + "px",
+		left: $currentItem.position().left + 5 + "px"
+    	});
+	}
+
+	// 下線のトランジション
+	$nav.find("li").hover(
+		function () {
+			$slideLine.css({
+	        width: $(this).width() + 10 + "px",
+	        left: $(this).position().left + 5 + "px"
+				});
+	    },
+	    function () {
+			if ($currentItem.length) {
+				// 現在の項目に戻す
+				$slideLine.css({
+				width: $currentItem.width() + 10 + "px",
+				left: $currentItem.position().left + 5 + "px"
+				});
+			} else {
+				// 非表示にする
+				$slideLine.width(0);
+			}
+		}
+	);
+}
+
 function showDate() {
-  const area = document.getElementById("date");
+	const area = document.getElementById("date");
 
-  const currentDate = new Date();
+	const currentDate = new Date();
   
-  const date = currentDate.toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
+	const date = currentDate.toLocaleDateString("ja-JP", {
+	    year: "numeric",
+	    month: "2-digit",
+	    day: "2-digit",
 	});
   
-  const weekday = currentDate.toLocaleDateString("ja-JP", {
-    weekday: "short",
+	const weekday = currentDate.toLocaleDateString("ja-JP", {
+    	weekday: "short",
 	});
 
-  const formatted = `${date}` + `（${weekday}）`;
+	const formatted = `${date}` + `（${weekday}）`;
 			
-  area.textContent = `${formatted}`;
+	area.textContent = `${formatted}`;
 }
 
 function showTime() {
@@ -26,12 +62,14 @@ function showTime() {
 	    hour: "2-digit",
 	    minute: "2-digit",
 	    second: "2-digit"
-	  });
+	});
 	
 	area.textContent = `${formatted}`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+	setupNavigationUnderline();
+
 	showDate();
  	showTime();
  	setInterval(showTime, 1000);
