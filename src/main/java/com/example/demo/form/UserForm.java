996bf012f4e643cc83960c3a8e85d6fa6cabdac0
kirasayaka <kirasayaka@localhost>
2025-11-29 16:28:56 +0900

    ユーザー登録（バリデーション）

--- src/main/java/com/example/demo/form/UserForm.java
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
@@ -15,27 +17,41 @@ public class UserForm implements ValidationGroups {
 	private Long id;
 
     @NotBlank(message = "名前(正式表示)を入力してください。")
+    @Size(min = 1, max = 255, message = "名前(正式表示)は、1文字以上、255文字以内で入力してください。")
     private String fnJp;
 
     @NotBlank(message = "名前(ひらがな)を入力してください。")
+    @Size(min = 1, max = 255, message = "名前(ひらがな)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "^[あ-ん]*$", message = "名前(ひらがな)は全角ひらがなで入力してください。")
     private String fnJpHira;
 
     @NotBlank(message = "名前(カタカナ)を入力してください。")
+    @Size(min = 1, max = 255, message = "名前(カタカナ)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "^[ア-ン]*$", message = "名前(カタカナ)は全角カタカナで入力してください。")
     private String fnJpKata;
 
     @NotBlank(message = "名前(英語)を入力してください。")
+    @Size(min = 1, max = 255, message = "名前(英語)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "[a-zA-Z]*", message = "名前(英語)は半角英字で入力してください")
     private String fnEn;
 
     @NotBlank(message = "姓(正式表示)を入力してください。")
+    @Size(min = 1, max = 255, message = "姓(正式表示)は、1文字以上、255文字以内で入力してください。")
     private String lnJp;
 
     @NotBlank(message = "姓(ひらがな)を入力してください。")
+    @Size(min = 1, max = 255, message = "姓(ひらがな)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "^[あ-ん]*$", message = "姓(ひらがな)は全角ひらがなで入力してください。")
     private String lnJpHira;
 
     @NotBlank(message = "姓(カタカナ)を入力してください。")
+    @Size(min = 1, max = 255, message = "姓(カタカナ)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "^[ア-ン]*$", message = "姓(カタカナ)は全角カタカナで入力してください。")
     private String lnJpKata;
 
     @NotBlank(message = "姓(英語)を入力してください。")
+    @Size(min = 1, max = 255, message = "姓(英語)は、1文字以上、255文字以内で入力してください。")
+    @Pattern(regexp = "[a-zA-Z]*", message = "姓(英語)は半角英字で入力してください")
     private String lnEn;
 
     // 旧姓の入力が必要な場合にチェック
@@ -51,14 +67,15 @@ public class UserForm implements ValidationGroups {
 
     @NotBlank(message = "メールアドレスを入力してください。")
     @Email(message = "メールアドレスの形式が正しくありません。")
-    @Size(max = 255, message = "メールアドレスは255文字以内で入力してください。")
+    @Size(min = 1, max = 255, message = "メールアドレスは1文字以上、255文字以内で入力してください。")
     private String email;
 
     @NotBlank(message = "パスワードを入力してください。")
-    @Size(min = 4, max = 255, message = "パスワードは4文字以上255文字以内で入力してください。")
+    @Size(min = 8, max = 255, message = "パスワードは8文字以上、255文字以内で入力してください。")
     private String password;
 
     @NotNull(message = "社員番号を入力してください。")
+    @Pattern(regexp = "^[0-9]+$", message = "社員番号は半角数字で入力してください")
     private Integer employeeNo;
 
     private Integer currentEmployeeNo;
